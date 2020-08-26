c33f2226e5d2d17380361d32bf2d449a37237740
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2020-08-26 12:47:47 +0700

    My own personal customized Todo App based on master branch

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+/** @jsx jsx */
+import { useState } from "react";
+import { jsx, css } from "@emotion/core";
+// import { styled } from "@emotion/styled";
+
 import { useTasks } from "../utils/TaskProvider";
 
 const NewTaskForm = () => {
@@ -7,12 +11,36 @@ const NewTaskForm = () => {
 
   const submit = (e) => {
     e.preventDefault();
-    addTask("task");
+    addTask(task);
     setTask("");
   };
 
-  return (
-    <form onSubmit={submit}>
+  const style = css`
+    width: 100%;
+    display: flex;
+
+    input[type="text"] {
+      height: 30px;
+      width: 100%;
+      border: 1px solid #ccc;
+      border-radius: 5px 0 0 5px;
+      padding: 5px;
+      outline-color: #42383b;
+      font-size: 14pt;
+    }
+
+    button {
+      background-color: #42383b;
+      color: #fff;
+      border: 0;
+      padding: 0 20px;
+      border-radius: 0px 5px 5px 0px;
+      font-size: 14pt;
+    }
+  `;
+
+  const Form = () => (
+    <form onSubmit={submit} css={style}>
       <input
         type="text"
         value={task}
@@ -22,6 +50,46 @@ const NewTaskForm = () => {
       />
       <button type="submit">Add</button>
     </form>
+  );
+
+  // const styledForm = styled.form`
+  //   form {
+  //     width: 100%;
+  //     display: flex;
+
+  //     input[type="text"] {
+  //       height: 30px;
+  //       width: 100%;
+  //       border: 1px solid #ccc;
+  //       border-radius: 5px 0 0 5px;
+  //       padding: 5px;
+  //       outline-color: #42383b;
+  //       font-size: 14pt;
+  //     }
+
+  //     button {
+  //       background-color: #42383b;
+  //       color: #fff;
+  //       border: 0;
+  //       padding: 0 20px;
+  //       border-radius: 0px 5px 5px 0px;
+  //       font-size: 14pt;
+  //     }
+  //   }
+  // `;
+
+  return (
+    // <Form onSubmit={submit}>
+    //   <input
+    //     type="text"
+    //     value={task}
+    //     placeholder="What you need to do?"
+    //     onChange={(e) => setTask(e.target.value)}
+    //     required
+    //   />
+    //   <button type="submit">Add</button>
+    // </Form>
+    <Form />
   );
 };
 
